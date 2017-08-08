7cbe9ff03848ecc55f492e6725c4a0bd0d8b0689
root <root@localhost.localdomain>
2017-08-08 19:44:03 +0200

    This is Version 1.0

---
+++ upgrader.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apt-get update | grep upgraded
+apt-get upgrade -y | grep upgraded
+apt-get dist-upgrade -y | grep upgraded
+
+sleep 2s
+
+echo Do you want to reboot? \(Y/N\)
+
+read YorN
+
+if [ $YorN = "Y" ]
+then
+	echo Rebooting...
+	reboot
+else
+	echo Goodbye
+	exit 0
+fi
